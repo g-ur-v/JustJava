593f945523969991170bc8a12e49360022d09672
GauravRawal <gaurav.rawal66@gmail.com>
2017-06-09 16:43:25 +0530

    Updated on 4:43pm

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,13 +1,41 @@
+/**
+ * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ */
 package com.example.android.justjava;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
+    int i=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(""+ i++);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     * @param number
+     */
+    private void display(String number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
