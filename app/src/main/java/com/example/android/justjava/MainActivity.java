f255a4b87f94ac775143a38d270d1796415884f4
GauravRawal <gaurav.rawal66@gmail.com>
2017-06-11 12:33:53 +0530

    .

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,7 +1,3 @@
-/**
- * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
- * This is the package name our example uses:
- */
 package com.example.android.justjava;
 
 
@@ -10,13 +6,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-    int i=0;
 
+    int noOfCoffees=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,15 +24,48 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(""+ i++);
+        String priceMessage = "Free";
+        displayMessage(priceMessage);
+    }
+
+    /*
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(""+ number);
+    }
+
+    /*
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    /*
+    * This method is used to add the functionality to the increment button.
+    * */
+    public void increment(View view){
+        noOfCoffees++;
+        display(noOfCoffees);
+        displayPrice(noOfCoffees*5);
+    }
+
+    /*
+    * This method is used to add the functionality to the increment button.
+    * */
+    public void decrement(View view){
+        noOfCoffees--;
+        display(noOfCoffees);
+        displayPrice(noOfCoffees*5);
     }
 
     /**
-     * This method displays the given quantity value on the screen.
-     * @param number
+     * This method displays the given text on the screen.
      */
-    private void display(String number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
