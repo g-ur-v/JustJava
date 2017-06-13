39977f0cfbc7cb1bcfc47f87e041214ca124ba0e
Gaurav Rawal <gaurav.rawal66@gmail.com>
2017-06-13 17:37:36 +0530

    Final Commit

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,7 +4,9 @@ package com.example.android.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import java.text.NumberFormat;
 
@@ -24,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Free";
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        String priceMessage = calculateOrderSummary(noOfCoffees,hasWhippedCream);
         displayMessage(priceMessage);
     }
 
@@ -37,19 +41,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-    /*
     * This method is used to add the functionality to the increment button.
     * */
     public void increment(View view){
         noOfCoffees++;
         display(noOfCoffees);
-        displayPrice(noOfCoffees*5);
     }
 
     /*
@@ -58,14 +54,20 @@ public class MainActivity extends AppCompatActivity {
     public void decrement(View view){
         noOfCoffees--;
         display(noOfCoffees);
-        displayPrice(noOfCoffees*5);
     }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
+    /*
+    This method canculate the price of the person
+     */
+    private String calculateOrderSummary(int number, boolean hasOrNot) {
+        return("Name: Kaptain Kunal\nQuantity: "+ number + "\nWhipped Cream Checked: "+ hasOrNot +"\nTotal: $"+ (number*5) + "\nThank You!");
+    }
+
 }
